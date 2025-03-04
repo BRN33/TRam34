d2d9a16f5916783eacce6cb804e1fc92ca4f4a1f
Uğur BARAN <ugur.baran@metro.istanbul>
2025-03-04 07:50:52 +0300

    Commit

--- LogicManager.API/Program.cs
+++ LogicManager.API/Program.cs
@@ -18,35 +18,47 @@ namespace LogicManager.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            var appHost = builder.Configuration.GetSection("ConnectionStrings").GetSection("Urls").Value!;
+
+            builder.WebHost.UseUrls(appHost);//Dýs ip de calýsacaksa bunu acýyoruz
+
             // Add services to the container.
             builder.Services.AddControllers();
 
             // Gerekli servislerin DI kaydý
 
-           
+
             builder.Services.Configure<MongoDbSettings>(builder.Configuration.GetSection("MongoDb"));
             builder.Services.AddSingleton<IMongoDbService, MongoDbService>();
 
             //builder.Services.AddSingleton<ITcmsService, TcmsService>();
-            builder.Services.AddSingleton<LeadershipManager>();//Amster Slave yapýsý icin
+            builder.Services.AddSingleton<LeadershipManager>();//Master Slave yapýsý icin
 
             builder.Services.AddSingleton<IAnonsService, AnonsService>();
             builder.Services.AddSingleton<ILedService, LedService>();
             builder.Services.AddSingleton<ILcdService, LcdService>();
             builder.Services.AddSingleton<ITakoReaderService, TakoReaderService>();
             builder.Services.AddSingleton<IRouteService, RouteService>();
-            builder.Services.AddSingleton<ITrainManagement, LogicManager.Infrastructure.Services.TrainManagement>();
+            builder.Services.AddSingleton<ITrainManagement, TrainManagement>();
 
 
             builder.Services.AddHostedService<TakoDataCommand>();
             builder.Services.AddHttpClient<TakoReaderService>();
             builder.Services.AddHttpClient<LoggerHelper>();
 
+
+            //Console da log seviyesini belirleme
+            builder.Logging.ClearProviders();
+            builder.Logging.AddConsole();
+            builder.Logging.SetMinimumLevel(LogLevel.Warning);
+            builder.Logging.AddFilter("System.Net.Http.HttpClient", LogLevel.None); // Sadece HttpClient loglarýný gizler
+
+
             builder.Services.AddControllers()
-                .AddJsonOptions(options =>
-                {
-                    options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-                });
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+            });
 
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
